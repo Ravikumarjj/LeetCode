7e0b52e67d863d0d4fe8a3adba4b56cec7aae235
Ravi kumar kanu baniya <87134236+Ravikumarjj@users.noreply.github.com>
2022-11-15 02:40:22 +0530

    Add files via upload

---
+++ find-value-of-variable-after-operation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X=0;
+        for ( int i = 0 ; i< operations.size() ; i++){
+            if( operations[i]=="++X" || operations[i]=="X++"){
+                X++;
+            }
+            else if(operations[i]=="--X" || operations[i]=="X--" ){
+                X--;
+            }
+            
+        }
+        return X;
+    }
+};
