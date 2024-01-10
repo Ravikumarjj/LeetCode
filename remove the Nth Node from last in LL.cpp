4ffeabb5f3d956e46a7755344c65bdc57a9c022f
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-10 23:12:07 +0530

    remove nth node from last in LL

---
+++ remove the Nth Node from last in LL.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+        if(fast==NULL) return head->next;
+        ListNode* slow = head;
+        while(fast->next){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next=slow->next->next;
+        delete delNode;
+        return head;
+    }
+};
