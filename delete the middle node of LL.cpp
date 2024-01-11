88c275d297821760c2b3d47dc398a8330511cb66
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-11 14:19:01 +0530

    delete the middle node of LL

---
+++ delete the middle node of LL.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast=fast->next->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next=slow->next->next;
+        delete delNode;
+        return head;
+        
+    }
+};
