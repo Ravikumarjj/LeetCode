6313217c437a078871e63b49aad07dd3eff91f84
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-19 14:42:02 +0530

    modified for n<=0

--- check whether the number is power of 2.cpp
+++ check whether the number is power of 2.cpp
@@ -3,5 +3,6 @@ using namespace std;
 bool isPowerOfTwo(int n)
 {	
     // n&n-1==0 if power of 2 and n>0 hence we need to check whether the n is 0 or not
-    return n && (!(n&(n-1)));
+    if(n<=0) return false;
+    return ((n&(n-1))==0);
 }
