db5c2d3925badde68878d29689eeab8e48b06b3f
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-21 18:15:06 +0530

    house robber

---
+++ house robber.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int a = 0;
+        int b = 0;
+        int n = nums.size();
+        for (int i=0; i<n; i++)
+        {
+            if (i%2==0)
+            {
+                a = max(a+nums[i], b);
+            }
+            else
+            {
+                b = max(a, b+nums[i]);
+            }
+        }
+
+        return max(a, b);
+    }
+};
