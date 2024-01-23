b3c6d61c33bc9c1d83e37cd912e7643aba662593
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-23 15:10:31 +0530

    subset

---
+++ subset.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans;
+	int n = nums.size();
+	for(int i=0;i<(1<<n);i++){
+		vector<int> s;
+		for(int j=0;j<n;j++){
+              if(i&(1<<j)) s.push_back(nums[j]);
+		}
+		ans.push_back(s);
+	}
+	return ans;
+    }
+};
