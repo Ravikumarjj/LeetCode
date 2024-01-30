4ff945f8e694eb4543a6016f676cf9d88ca5bad4
Ravi kanu baniya <geniusk53@gmail.com>
2024-01-30 10:10:18 +0530

    subset sum II

---
+++ subset sum II.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(int ind,vector<int>& ds,vector<int>& arr,vector<vector<int>>& ans){
+        ans.push_back(ds);
+        for(int i = ind;i<arr.size();i++){
+            if(i>ind && arr[i]==arr[i-1])continue;
+            ds.push_back(arr[i]);
+            solve(i+1,ds,arr,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums){
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        solve(0,ds,nums,ans);
+        return ans;
+    }
+    
+};
