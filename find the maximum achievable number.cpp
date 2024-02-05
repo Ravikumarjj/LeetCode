719229fee85a8ad7e0842e3da70e4a9253b047c1
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-05 19:46:15 +0530

    find the maximum achievable number

---
+++ find the maximum achievable number.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    //for num and t, ans is achievable is t no. of operation done in both num and ans to get them equal
+    int theMaximumAchievableX(int num, int t) {
+        return num+2*t;
+        
+    }
+};
