b29fd22fa4f048216a17807dce81efd2fa9161e5
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-05 20:08:25 +0530

    count the number of matches

---
+++ count the number of matches.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+       return n-1;//as there will be one team that will not lose ans other n-1 will lose so total match is n-1
+    }
+};
