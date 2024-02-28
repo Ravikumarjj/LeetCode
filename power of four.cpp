8b36569aeb73342c39d3a985c25792a817ee35a9
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-28 12:33:34 +0530

    power of four

---
+++ power of four.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0)return false;
+        while(n>1){
+            if(n%4!=0)return false;
+            n/=4;
+        }
+        return true;
+    }
+};
